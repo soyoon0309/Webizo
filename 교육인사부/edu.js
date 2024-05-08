dedb00ed75d8ada36be2fb61bcd4055a61608621
9xxng <qowop19@gmail.com>
2024-05-08 20:07:36 +0900

    교육인사부 수정

--- 교육인사부/edu.js
+++ 교육인사부/edu.js
@@ -11,4 +11,17 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
+
+    const submenus = document.querySelectorAll('.submenu a');
+    submenus.forEach(menu => {
+        menu.addEventListener('click', function(event) {
+            event.preventDefault(); // 하이퍼링크 기본 동작 방지
+            submenus.forEach(sub => sub.classList.remove('active'));
+            menu.classList.add('active'); // 현재 클릭된 메뉴에 active 클래스 추가
+            window.location.href = menu.href; // 페이지 이동
+        });
+    });
+
+    const menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach
 });
